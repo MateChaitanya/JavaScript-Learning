816a02b6a773fe5de796acf39493707e50fbff33
MateChaitanya <chaitanyamate02@gmail.com>
2024-03-03 23:41:36 +0530

    first commit

--- first.js
+++ first.js
@@ -15,7 +15,7 @@ console.log(age);*/
 student["age"] = student["age"] + 1;
 console.log(student["age"]);*/
 
-
+/*
 const product = {
     title:"Ball Pen",
     rating: 4,
@@ -23,3 +23,35 @@ const product = {
     price : 270,
 };
 console.log(product);
+*/
+
+/*const profile = {
+    username:"@shradhakhapra",
+    isfollow:false,
+    follower:123,
+    following:123
+};
+console.log(profile);*/
+
+//------------------------------------------------------------------------------------
+
+//Arithmetic operation
+/*
+let a=5;
+let b=2;
+
+console.log("a = ", a, " &  b = ",b);
+console.log("a + b =",a+b);
+console.log("a - b =",a-b);
+console.log("a * b =",a*b);*/
+
+//Unary Operator
+let a = 5;
+let b = 2;
+
+a += 4; // a = a+4
+console.log("a = ", a);
+
+console.log("a = ", a, " & b = ",b);
+console.log("a++ =" , a++);
+console.log("a = ",a)
