9df0d0f764fecf325e7fbc5d763d1ea346b3128c
MateChaitanya <chaitanyamate02@gmail.com>
2024-03-04 08:53:26 +0530

    first commit

--- first.js
+++ first.js
@@ -57,8 +57,32 @@ console.log("a++ =" , a++);
 console.log("a = ",a)*/
 
 //logical Opertaor
-
+/*
 let a = 6;
 let b = 5;
 
-console.log("cond1 && cond2 = ",a<b || a==6);
+console.log("cond1 && cond2 = ",a<b || a==6);*/
+
+//Conditional Statements
+ 
+ let mode = "dark";
+ let color;
+
+ if(mode === "dark"){
+    color ="black";
+ }
+
+ if(mode === "light"){
+    color = "white";
+ }
+
+ console.log(color);
+ 
+//let age = 16;
+// if(age>18){
+//     console.log("You can vote");
+// }
+
+// if(age<18){
+//     console.log("you can not vote");
+// }
