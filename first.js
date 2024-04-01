5be458880a2df7514c3c6d0f3d7d1e49d4291a48
MateChaitanya <chaitanyamate02@gmail.com>
2024-04-01 11:23:52 +0530

    Initial commit

--- first.js
+++ first.js
@@ -137,19 +137,33 @@ else{
 
 
 
-//Calculate sum of 1 to 5 
+// //Calculate sum of 1 to 5 
 
-let sum = 0;
-for(let i = 1; i <= 5; i++){
-    sum = sum + i;
-}
-console.log("sum = ",sum);
-console.log("loop has ended");
+// let sum = 0;
+// for(let i = 1; i <= 5; i++){
+//     sum = sum + i;
+// }
+// console.log("sum = ",sum);
+// console.log("loop has ended");
 
-//Print 1 to 5 
+// //Print 1 to 5 
 
-for(let i = 1; i <=5; i++){
-    console.log("i = ", i);
-}
-console.log(i);
+// for(var i = 1; i <=5; i++){
+//     console.log("i = ", i);
+// }
+
+// console.log(i); 
+
+
+// let i = 1;
+// while(i<=5){
+//     console.log("i=",i);
+//     i++;
+// }
+
+let i = 1;
+do{
+    console.log("i=",i);
+    i++;
+}while ( i<=10);
 
