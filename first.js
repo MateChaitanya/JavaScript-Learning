80eef35bf339ae439a0c0a3c86ac6ce4914a5ae8
MateChaitanya <chaitanyamate02@gmail.com>
2024-04-01 11:39:22 +0530

    Initial commit

--- first.js
+++ first.js
@@ -169,7 +169,22 @@ else{
 
 //Practice Qs1
 for( let num = 0 ; num <=100; num ++){
-    if(num%2 ===0){
+    if(num%2 ===0){ //even number 
         console.log("num =", num);
     }
 }
+
+for( let num = 0 ; num <=100; num ++){
+    if(num%2 !==0){ //odd number 
+        console.log("num =", num);
+    }
+}
+
+//Practice Qs2
+
+let gameNum = 25;
+let userNum = prompt("Guess the game number : ");
+while(userNum != gameNum){
+    userNum =  prompt("You enterd wrong number. Guess again ")
+}
+console.log("Congratulations, you entered the right number");
