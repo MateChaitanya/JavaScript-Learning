5699b5f41da1993475719cec8b7a8ab3446c76bf
MateChaitanya <chaitanyamate02@gmail.com>
2024-04-03 10:00:16 +0530

    Initial commit

--- JavaScriptDOM/script.js
+++ JavaScriptDOM/script.js
@@ -1,5 +1,15 @@
 /*console.log("hello");
 alert("Apna College");*/
 
-console.dir(document.body);
-console.log(document.body);
+/*console.dir(document.body);
+console.log(document.body);*/
+
+/*let heading = document.getElementById("heading");
+console.dir (heading);*/
+
+/*let heading = document.getElementsByClassName("heading-class");
+console.dir(heading);
+console.log(heading);*/
+
+let parahs = document.getElementsByTagName("p");
+console.dir(parahs);
