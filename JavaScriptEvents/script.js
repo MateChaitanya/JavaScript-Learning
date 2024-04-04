c58dcb1a04de62d04c948101d83540dc7269c5fa
MateChaitanya <chaitanyamate02@gmail.com>
2024-04-04 11:25:58 +0530

    Initial commit

--- JavaScriptEvents/script.js
+++ JavaScriptEvents/script.js
@@ -1,10 +1,12 @@
  let btn1 = document.querySelector("#btn1");
 
- btn1.onclick =() => {
-    console.log("btn1 was Clicked");
+ btn1.onclick =(e) => {
+    console.log(e);
+    console.log(e.type);
+    /*console.log("btn1 was Clicked");
     let a =25;
     a++;
-    console.log(a);
+    console.log(a);*/
  };
 
  let div = document.querySelector("div");
